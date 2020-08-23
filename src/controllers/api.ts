1d5921248cff042dd3d4fb3df2e96360130a6daf
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-08-24 00:12:56 +0100

    shorten method built

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -3,7 +3,36 @@
 import graph from "fbgraph";
 import { Response, Request, NextFunction } from "express";
 import { UserDocument } from "../models/User";
+import { Z_ASCII } from "zlib";
 
+
+const shorten = (num: number) => {
+	const mapAlphanumric = [
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+	];
+
+	let ds:number[] = [];
+	
+	while (num > 0) {
+		const remainder = num % 36;
+		ds.push(remainder);
+		num = Math.floor(num / 36);
+	}
+	ds = ds.reverse();
+	if (ds.length < 8) {
+		while (ds.length != 8) {
+			ds.unshift(0);
+		}
+	}
+
+	const pathname = [];
+	ds.forEach(r => {
+		pathname.push(mapAlphanumric[r]);
+	});
+
+	return ds.join();
+}
 
 /**
  * List of API examples.
