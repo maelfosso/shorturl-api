9503d12cbce362d0c56d9275e436659968dbc564
Mael FOSSO <fosso.mael.elvis@gmail.com>
2020-08-23 13:39:33 +0100

    Simple URL test written

---
+++ test/models/url.test.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+import { Url, UrlDocument } from '../../src/models/Url';
+
+describe("Tests concerning User model", () => {
+
+  beforeAll(async () => {
+    await mongoose.connect('mongodb://localhost:27017/shorten_test', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true } ).then(
+      () => { /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ },
+    ).catch(err => {
+        console.log(`MongoDB connection error. Please make sure MongoDB is running. ${err}`);
+        // process.exit();
+    });
+  });
+
+  it ('creates and saves Url successfully', async () => {
+    const valid = new Url({
+      originalURL: "http://pbid.io/new-file-hew-ue",
+      shortenURL: "http://pbid.io/u2defa8w"
+    })
+    const saved = await valid.save();
+
+    expect(saved._id).toBeDefined();
+    expect(saved.originalURL).toBe(valid.originalURL);
+    expect(saved.shortenURL).toBe(valid.shortenURL);
+  })
+});
